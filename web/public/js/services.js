abe434c5cbfeef029b4621194b066f5c2e340d10
loopyz <lguo@andrew.cmu.edu>
2014-09-13 03:31:14 -0400

    web stuff

---
+++ web/public/js/services.js
@@ -0,0 +1,12 @@
+(function() {
+   'use strict';
+
+   /* Services */
+
+   angular.module('edugames.services', ['edugames.service.login', 'edugames.service.firebase'])
+
+      // put your services here!
+      // .service('serviceName', ['dependency', function(dependency) {}]);
+
+})();
+
