211b086d32245d4008ecfb5c6456b82f7fb70ee6
loopyz <lguo@andrew.cmu.edu>
2014-09-13 20:16:15 -0400

    made height prettier

--- web/public/js/temp.js
+++ web/public/js/temp.js
@@ -1,5 +1,5 @@
 function stopDemHaterz() {
-    $("#editor").css("height", $(window).height()*.90);
+    $("#editor").css("height", $(window).height()*.83);
     $("#singleeditor").css("height", $(window).height()*.90);
-    $("#output").css("height", $(window).height()*.90);
+    $("#output").css("height", $(window).height()*.83);
 }
